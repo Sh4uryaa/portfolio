44b0ad37fd7dae9a94b34516b4939d8138d88a62
Sh4uryaa <shaurya.mishra_@outlook.com>
2026-07-22 19:24:41 +0530

    Add hamburger navbar for smaller sizes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let navbar = document.querySelector("header");
+const navbar = document.querySelector("header");
 window.addEventListener("scroll", () => {
     if(window.scrollY > 608)
     {
@@ -6,4 +6,12 @@ window.addEventListener("scroll", () => {
     }else {
         navbar.classList.remove("scrolled");
     }
-})
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector("header ul");
+
+hamburger.addEventListener("click", () => navLinks.classList.toggle("nav-open"));
+
+navLinks.addEventListener("click", () => navLinks.classList.remove("nav-open"));
+
