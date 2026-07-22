f09a65c7e7b1ea3dc168a6b3b779d77d9b8660b4
Sh4uryaa <shaurya.mishra_@outlook.com>
2026-07-22 20:04:14 +0530

    Add dynamic texts in contacts

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const navbar = document.querySelector("header");
+const phrases = ["chat :)", "talk sports.", "discuss movie theories.", "be my friend :D"]
 window.addEventListener("scroll", () => {
     if(window.scrollY > 608)
     {
@@ -15,3 +16,33 @@ hamburger.addEventListener("click", () => navLinks.classList.toggle("nav-open"))
 
 navLinks.addEventListener("click", () => navLinks.classList.remove("nav-open"));
 
+let phraseIndex = 0;
+let charIndex = 0;
+const changingText = document.querySelector("#changing-text")
+
+
+function type() {
+    changingText.textContent = phrases[phraseIndex].slice(0, charIndex);
+    charIndex++;
+
+    if(charIndex <= phrases[phraseIndex].length) {
+        setTimeout(type, 100);
+    } else {
+        setTimeout(erase, 1000);
+    }
+}
+
+function erase() {
+    changingText.textContent = phrases[phraseIndex].slice(0, charIndex);
+    charIndex--;
+
+    if(charIndex >= 0) {
+        setTimeout(erase, 50);
+    } else {
+        phraseIndex = (phraseIndex + 1) % phrases.length;
+        charIndex = 0;
+        setTimeout(type, 300);
+    }
+}
+
+type();
